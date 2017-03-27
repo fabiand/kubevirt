9e1b71e70559e6fe716e43fe2ebbcc1e781a6404
Fabian Deutsch <fabiand@fedoraproject.org>
2017-03-27 10:24:34 +0200

    sync imp

--- cluster/sync.sh
+++ cluster/sync.sh
@@ -5,7 +5,7 @@ cluster/vagrant/sync_config.sh
 
 make all DOCKER_TAG=devel
 
-for VM in `vagrant status | grep -v "^The Libvirt domain is running." | grep running | cut -d " " -f1`; do
+for VM in `vagrant status | grep "running (libvirt)" | cut -d " " -f1`; do
   vagrant rsync $VM # if you do not use NFS
   vagrant ssh $VM -c "cd /vagrant && export DOCKER_TAG=devel && sudo -E hack/build-docker.sh"
 done
